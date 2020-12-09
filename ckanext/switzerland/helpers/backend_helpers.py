0ff17dcc3691f63eecc4708f4eb378a2e4188722
Rae Knowler <rae.knowler@liip.ch>
2020-12-09 11:35:04 +0100

    Monkey patch helpers to translate names/titles

--- ckanext/switzerland/helpers/backend_helpers.py
+++ ckanext/switzerland/helpers/backend_helpers.py
@@ -4,11 +4,16 @@
 Helpers belong in this file if they are only
 used in backend templates
 """
+import ast
 import logging
 from urlparse import urlparse
 from ckan.common import session
 from ckan.authz import auth_is_loggedin_user
 from ckan.common import _
+from ckan.lib.helpers import _link_to, url_for, lang
+from ckan.lib.helpers import dataset_display_name as dataset_display_name_orig
+from ckan.lib.helpers import organization_link as organization_link_orig
+
 import ckan.lib.i18n as i18n
 import ckan.logic as logic
 import ckan.plugins.toolkit as tk
@@ -190,3 +195,63 @@ def ogdch_resource_display_name(res):
                 logic.NotAuthorized, AttributeError):
             return ""
     return resource_display_name
+
+
+# monkey patched version of ckan.lib.helpers.dataset_display_name which extracts the correct translation of the dataset
+def dataset_display_name(package_or_package_dict):
+    name = dataset_display_name_orig(package_or_package_dict)
+    name = get_localized_value_for_display(name)
+    return name
+
+
+# monkey patched version of ckan.lib.helpers.resource_display_name which extracts the correct translation of the dataset
+def resource_display_name(resource_dict):
+    name = resource_dict.get('name', None)
+    description = resource_dict.get('description', None)
+    if name:
+        name = get_localized_value_for_display(name)
+        return name
+    elif description:
+        description = get_localized_value_for_display(description)
+        if isinstance(description, basestring):
+            description = description.split('.')[0]
+            max_len = 60
+            if len(description) > max_len:
+                description = description[:max_len] + '...'
+        return description
+    else:
+        return _("Unnamed resource")
+
+
+# monkey patched version of ckan.lib.helpers.organization_link which extracts the correct translation of the org
+def organization_link(organization):
+    organization['title'] = get_localized_value_for_display(organization['title'])
+    return organization_link_orig(organization)
+
+
+# monkey patched version of ckan.lib.helpers.group_link which extracts the correct translation of the dataset
+def group_link(group):
+    url = url_for(controller='group', action='read', id=group['name'])
+    title = group['title']
+    title = get_localized_value_for_display(title)
+    # the group creation message contains str(dict), so we must parse the string to fix it
+    title = ast.literal_eval(title)
+    return _link_to(title, url)
+
+
+# patch activity
+def resource_link(resource_dict, package_id):
+    # ---
+    # issue: resource_dict['name'] is saved as str(dict), and therefore is invalid json
+    #   -> parse_json just returns the string
+    # resolutions: parse the invalid json string into a dict
+    # ---
+    if 'name' in resource_dict and resource_dict['name']:
+        resource_dict['name'] = get_localized_value_for_display(ast.literal_eval(resource_dict['name']))
+
+    text = resource_display_name(resource_dict)
+    url = url_for(controller='package',
+                  action='resource_read',
+                  id=package_id,
+                  resource_id=resource_dict['id'])
+    return _link_to(text, url)
--- ckanext/switzerland/plugins.py
+++ ckanext/switzerland/plugins.py
@@ -11,6 +11,7 @@ import ckanext.switzerland.helpers.plugin_utils as ogdch_plugin_utils
 import ckanext.switzerland.helpers.request_utils as ogdch_request_utils
 import ckanext.switzerland.helpers.localize_utils as ogdch_localize_utils
 import ckanext.switzerland.helpers.format_utils as ogdch_format_utils
+import ckan.lib.helpers as h
 import ckan.plugins as plugins
 from ckan.lib.plugins import DefaultTranslation
 import ckanext.xloader.interfaces as ix
@@ -202,6 +203,14 @@ class OgdchPlugin(plugins.SingletonPlugin, DefaultTranslation):
                     action='xml_upload')
 
         return map
+
+
+# monkey patch template helpers to return translated names/titles
+h.dataset_display_name = ogdch_backend_helpers.dataset_display_name
+h.resource_display_name = ogdch_backend_helpers.resource_display_name
+h.group_link = ogdch_backend_helpers.group_link
+h.resource_link = ogdch_backend_helpers.resource_link
+h.organization_link = ogdch_backend_helpers.organization_link
 
 
 class OgdchMixin(object):
