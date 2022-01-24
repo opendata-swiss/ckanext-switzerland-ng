6f0ae1bb8aaeb7ad2429aa9f0014b13ca1c94de0
Maennel <sabine.maennel@liip.ch>
2022-01-24 17:42:01 +0100

    fix test of validator

--- ckanext/switzerland/tests/test_validators.py
+++ ckanext/switzerland/tests/test_validators.py
@@ -1,21 +1,12 @@
 # encoding: utf-8
-import datetime
 
 from nose.tools import assert_equals, assert_raises
-from dateutil.parser import parse, ParserError
+from dateutil.parser import parse
 
 from ckan.plugins.toolkit import get_validator, Invalid
-from ckan import plugins
 
 
 class TestIValidators(object):
-    @classmethod
-    def setup_class(cls):
-        plugins.load('ogdch')
-
-    @classmethod
-    def teardown_class(cls):
-        plugins.unload('ogdch')
 
     def test_ogdch_date_validator_timestamp_to_isodate(self):
         d = parse('2022-01-02')
