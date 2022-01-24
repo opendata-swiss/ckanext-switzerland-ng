d160115fd5bf6d01f3c199df4400a6a36de12f84
Maennel <sabine.maennel@liip.ch>
2022-01-24 17:34:38 +0100

    fix plugin name: it is found in the entry points

--- ckanext/switzerland/tests/test_validators.py
+++ ckanext/switzerland/tests/test_validators.py
@@ -11,11 +11,11 @@ from ckan import plugins
 class TestIValidators(object):
     @classmethod
     def setup_class(cls):
-        plugins.load('OgdchPlugin')
+        plugins.load('ogdch')
 
     @classmethod
     def teardown_class(cls):
-        plugins.unload('OgdchPlugin')
+        plugins.unload('ogdch')
 
     def test_ogdch_date_validator_timestamp_to_isodate(self):
         d = parse('2022-01-02')
