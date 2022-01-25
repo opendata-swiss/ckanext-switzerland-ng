2cff5bea5682f3ab35475520077162aca836130b
Maennel <sabine.maennel@liip.ch>
2022-01-25 06:38:54 +0100

    add helper functions for each expected date format
    the functions transform the date value into a storage or display date
    the target date format for storage is isodate
    the target date format for display is ogdch date

---
+++ ckanext/switzerland/helpers/date_helpers.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+import logging
+from dateutil.parser import parse
+import ckan.plugins.toolkit as tk
+
+DATE_FORMAT = tk.config.get(
+    'ckanext.switzerland.date_picker_format', '%d.%m.%Y')
+
+log = logging.getLogger(__name__)
+
+
+def get_isodate_as_isodate(value):
+    try:
+        value_as_isoformat = parse(value).isoformat()
+        if value_as_isoformat == value:
+            return value
+    except Exception:
+        return None
+
+
+def get_ogdch_date_as_isodate(value):
+    try:
+        return datetime.strptime(value, DATE_FORMAT).isoformat()
+    except Exception:
+        return None
+
+
+def get_timestamp_date_as_isodate(value):
+    try:
+        return datetime.fromtimestamp(int(value)).isoformat()
+    except Exception:
+        return None
+
+
+def get_ogdch_date_from_isodate(value):
+    try:
+        value_as_isoformat = parse(value).isoformat()
+        if value_as_isoformat == value:
+            return parse(value).strftime(DATE_FORMAT)
+    except Exception:
+        return None
+
+
+def get_ogdch_date_from_ogdch_date(value):
+    try:
+        value_datetime = datetime.strptime(value, DATE_FORMAT)
+        if value_datetime:
+            return value
+    except Exception:
+        return None
+
+
+def get_ogdch_date_from_timestamp(value):
+    try:
+        value_as_datetime = datetime.fromtimestamp(int(value))
+        return value_as_datetime.strftime(DATE_FORMAT)
+    except Exception:
+        return None
