8363c5270761916f6c7aab20ef158e5437b054b9
Maennel <sabine.maennel@liip.ch>
2022-01-25 15:09:07 +0100

    add more tests

--- ckanext/switzerland/tests/test_validators.py
+++ ckanext/switzerland/tests/test_validators.py
@@ -7,31 +7,75 @@ from datetime import datetime
 from ckan.plugins.toolkit import get_validator, Invalid
 
 
-class TestOgdchDateValidators(object):
+class TestOgdchDateStorageValidator(object):
     def setup(self):
         self.validator = get_validator('ogdch_date_validator')
 
     def test_ogdch_date_validator_parseeable_date_to_isodate(self):
         d = parse('2022-01-02')
-        d_isodate = d.isoformat()
-        assert_equals(d_isodate, self.validator(d))
+        d_storage = d.isoformat()
+        assert_equals(d_storage, self.validator(d))
 
     def test_ogdch_date_validator_isodate_to_isodate(self):
         d = parse('2022-01-02').isoformat()
-        assert_equals(d, self.validator(d))
+        d_storage = d
+        assert_equals(d_storage, self.validator(d))
 
     def test_ogdch_german_date_to_isodate(self):
         d = '02.04.2020'
-        d_isodate = parse(d, dayfirst=True).isoformat()
-        assert_equals(d_isodate, self.validator(d))
+        d_storage = parse(d, dayfirst=True).isoformat()
+        assert_equals(d_storage, self.validator(d))
 
     def test_timestamp_to_isodate(self):
         date_value = parse(str('04.05.2000'), dayfirst=True)
         epoch = datetime(1970, 1, 1)
         d = int((date_value - epoch).total_seconds())
-        d_isodate = date_value.isoformat()
-        assert_equals(d_isodate, self.validator(d))
+        d_storage = date_value.isoformat()
+        assert_equals(d_storage, self.validator(d))
 
     def test_ogdch_date_validator_isodate_before_1870_to_isodate(self):
         d = parse('1655-01-02').isoformat()
-        assert_equals(d, self.validator(d))
+        d_storage = d
+        assert_equals(d_storage, self.validator(d))
+
+    def test_ogdch_date_validator_datetime_to_isodate(self):
+        d = datetime(2022, 1, 2, 0, 0)
+        d_storage = d.isoformat()
+        assert_equals(d_storage, self.validator(d))
+
+
+class TestOgdchDateDisplayValidator(object):
+    def setup(self):
+        self.validator = get_validator('ogdch_date_output')
+
+    def test_ogdch_date_validator_parseeable_date_to_ogdch_date(self):
+        d = parse('2022-01-02')
+        d_display = '02.01.2022'
+        assert_equals(d_display, self.validator(d))
+
+    def test_ogdch_date_validator_isodate_to_ogdch_date(self):
+        d = parse('2022-01-02').isoformat()
+        d_display = '02.01.2022'
+        assert_equals(d_display, self.validator(d))
+
+    def test_ogdch_german_date_to_ogdch_date(self):
+        d = '02.04.2020'
+        d_display = d
+        assert_equals(d_display, self.validator(d))
+
+    def test_timestamp_to_ogdch_date(self):
+        date_value = parse(str('04.05.2000'), dayfirst=True)
+        epoch = datetime(1970, 1, 1)
+        d = int((date_value - epoch).total_seconds())
+        d_display = '04.05.2000'
+        assert_equals(d_display, self.validator(d))
+
+    def test_ogdch_date_validator_isodate_before_1870_to_ogdch_date(self):
+        d = parse('1655-01-02').isoformat()
+        d_display = '02.01.1655'
+        assert_equals(d_display, self.validator(d))
+
+    def test_ogdch_date_validator_datetime_to_ogdch_date(self):
+        d = datetime(2022, 1, 2, 0, 0)
+        d_display = '02.01.2022'
+        assert_equals(d_display, self.validator(d))
