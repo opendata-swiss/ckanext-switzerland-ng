ec64407f25082a4920cffe1ac9997e81e9cc7ecf
sabinem <sabine.maennel@liip.ch>
2022-11-21 14:51:59 +0100

    fix: spelling error in error message

--- ckanext/switzerland/logic.py
+++ ckanext/switzerland/logic.py
@@ -318,7 +318,7 @@ def ogdch_showcase_submit(context, data_dict):
     if not author_email:
         raise ValidationError("Missing author_email")
     if context.get('ratelimit_exceeded'):
-        raise ValidationError("Ratelimt for {} exceeded".format(author_email))
+        raise ValidationError("Rate limt for {} exceeded".format(author_email))
     try:
         title = data_dict.get('title')
         if not title:
