2bf0e9e42a00763f2a79ebd901f80ddf1fa6b9cb
Rae Knowler <rae.knowler@liip.ch>
2023-11-10 09:44:38 +0100

    tests: Add tests for license validator

--- ckanext/switzerland/tests/test_validators.py
+++ ckanext/switzerland/tests/test_validators.py
@@ -1,9 +1,8 @@
 # encoding: utf-8
 import json
 
-from ckan.lib.navl.dictization_functions import Invalid
 from ckan.plugins.toolkit import get_validator
-from nose.tools import assert_equals, assert_raises
+from nose.tools import assert_equals
 
 
 class TestOgdchUrlListValidator(object):
@@ -42,3 +41,86 @@ class TestOgdchUrlListValidator(object):
 
         assert_equals(value, data[key])
         assert_equals([u"Provided URL 'foobar' is not valid"], errors[key])
+
+
+class TestOgdchLicenseRequiredValidator(object):
+    license_key = (u'resources', 0, u'license')
+    rights_key = (u'resources', 0, u'rights')
+
+    def setup(self):
+        self.validator = get_validator('ogdch_license_required')(
+            'field', {}
+        )
+
+    def test_validate_license(self):
+        value = 'Creative Commons CC Zero License (cc-zero)'
+        key = (u'resources', 0, u'license')
+        data = {
+            key: value,
+        }
+        errors = {
+            key: [],
+        }
+        self.validator(key, data, errors, {})
+
+        assert_equals(value, data[key])
+        assert_equals([], errors[key])
+
+    def test_validate_rights(self):
+        value = None
+        key = (u'resources', 0, u'license')
+        rights_value = 'NonCommercialAllowed-CommercialAllowed-ReferenceRequired'
+        rights_key = (u'resources', 0, u'rights')
+
+        data = {
+            key: value,
+            rights_key: rights_value,
+        }
+        errors = {
+            key: [],
+        }
+        self.validator(key, data, errors, {})
+
+        assert_equals(
+            'NonCommercialAllowed-CommercialAllowed-ReferenceRequired',
+            data[key]
+        )
+        assert_equals([], errors[key])
+
+    def test_validate_both_license_and_rights(self):
+        value = 'Creative Commons CC Zero License (cc-zero)'
+        key = (u'resources', 0, u'license')
+        rights_value = 'NonCommercialAllowed-CommercialAllowed-ReferenceRequired'
+        rights_key = (u'resources', 0, u'rights')
+
+        data = {
+            key: value,
+            rights_key: rights_value,
+        }
+        errors = {
+            key: [],
+        }
+        self.validator(key, data, errors, {})
+
+        assert_equals(value, data[key])
+        assert_equals([], errors[key])
+
+    def test_validate_neither_licence_nor_rights(self):
+        value = None
+        key = (u'resources', 0, u'license')
+        rights_value = None
+        rights_key = (u'resources', 0, u'rights')
+        data = {
+            key: value,
+            rights_key: rights_value,
+        }
+        errors = {
+            key: [],
+        }
+        self.validator(key, data, errors, {})
+
+        assert_equals('', data[key])
+        assert_equals(
+            ["Distributions must have either 'rights' or 'license'"],
+            errors[key]
+        )
