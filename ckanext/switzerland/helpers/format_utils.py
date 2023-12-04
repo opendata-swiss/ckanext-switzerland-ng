0ddc19c0ce72fa7f0f26bcf26bcdaba2a0e571a0
Rae Knowler <rae.knowler@liip.ch>
2023-12-04 18:00:25 +0100

    feat: Map media_type to a value from IANA Media Type if there is one

--- ckanext/switzerland/helpers/format_utils.py
+++ ckanext/switzerland/helpers/format_utils.py
@@ -25,16 +25,20 @@ class FormatMappingNotLoadedError(Exception):
     pass
 
 
-def ogdch_get_format_mapping():
-    """reads in the format mapping from a yaml file"""
+def ogdch_get_format_mapping(mapping_type):
+    """Read in a format mapping from a yaml file
+    """
+    mapping_path = os.path.join(
+        __location__,
+        '{}_mapping.yaml'.format(mapping_type)
+    )
     try:
-        mapping_path = os.path.join(__location__, 'mapping.yaml')
-        with open(mapping_path, 'r') as format_mapping_file:
-            format_mapping = yaml.safe_load(format_mapping_file)
-            reverse_format_mapping = {}
-            for key, format_list in format_mapping.iteritems():
+        with open(mapping_path, 'r') as mapping_file:
+            mapping = yaml.safe_load(mapping_file)
+            reverse_mapping = {}
+            for key, format_list in mapping.iteritems():
                 for format in format_list:
-                    reverse_format_mapping[format] = key
+                    reverse_mapping[format] = key
     except (IOError, yaml.YAMLError) as exception:
         raise FormatMappingNotLoadedError(
             'Loading Format-Mapping from Path: (%s) '
@@ -42,21 +46,27 @@ def ogdch_get_format_mapping():
             % (mapping_path, exception)
         )
 
-    return format_mapping, reverse_format_mapping
+    return mapping, reverse_mapping
 
 
-format_mapping, reverse_format_mapping = ogdch_get_format_mapping()
+format_mapping, reverse_format_mapping = ogdch_get_format_mapping(
+    mapping_type='format'
+)
+media_type_mapping, reverse_media_type_mapping = ogdch_get_format_mapping(
+    mapping_type='media_type'
+)
 
 
 def prepare_resource_format(resource):
-    """determine resource format depending on
-    media_type, format and download_url
+    """Determine resource format depending on media_type, format and
+    download_url. Then convert media_type to its IANA value.
     """
     resource['format'] = get_resource_format(
         media_type=resource.get('media_type'),
         format=resource.get('format'),
         download_url=resource.get('download_url')
     )
+    resource['media_type'] = get_iana_media_type(resource.get('media_type'))
     return resource
 
 
@@ -95,6 +105,22 @@ def get_resource_format(media_type, format, download_url):
     return 'SERVICE'
 
 
+def get_iana_media_type(media_type):
+    """Get a value for media_type that belongs to the IANA Media Types
+    vocabulary, if there is one. Otherwise, return the media_type as it is.
+    """
+    if not media_type:
+        return ""
+
+    iana_media_type = _map_to_valid_media_type(
+        _get_cleaned_format_or_media_type(media_type)
+    )
+    if iana_media_type:
+        return iana_media_type
+
+    return media_type
+
+
 def prepare_formats_for_index(resources, linked_data_only=False):
     """Generate a deduplicated list with formats of all resources, or all
     Linked Data formats that are present.
@@ -116,9 +142,9 @@ def prepare_formats_for_index(resources, linked_data_only=False):
 
 
 def _map_to_valid_format(format, try_to_clean=False):
-    """check whether the format if in the mapping:
-    either as a key or as a value or if it can be derived
-    after cleaning the input format string"""
+    """Check whether the format is in the format mapping, either as a key or
+    as a value, or if it can be derived after cleaning the input format string
+    """
     if format in format_mapping.keys():
         return format
 
@@ -128,6 +154,22 @@ def _map_to_valid_format(format, try_to_clean=False):
         cleaned_format = _get_cleaned_format_or_media_type(format)
         if cleaned_format in reverse_format_mapping.keys():
             return reverse_format_mapping[cleaned_format]
+    return ""
+
+
+def _map_to_valid_media_type(media_type, try_to_clean=False):
+    """Check whether the media_type is in the mapping, either as a key or as a
+    value, or if it can be derived after cleaning the input media_type string
+    """
+    if media_type in media_type_mapping.keys():
+        return media_type
+
+    if media_type in reverse_media_type_mapping.keys():
+        return reverse_media_type_mapping[media_type]
+    if try_to_clean:
+        cleaned_media_type = _get_cleaned_format_or_media_type(media_type)
+        if cleaned_media_type in reverse_media_type_mapping.keys():
+            return reverse_media_type_mapping[cleaned_media_type]
     return ""
 
 
