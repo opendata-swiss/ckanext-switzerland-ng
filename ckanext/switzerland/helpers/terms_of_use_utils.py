8521fcac28b811212c0b89622190e992dbe90c2b
Rae Knowler <rae.knowler@liip.ch>
2024-08-08 13:55:47 +0200

    style: Remove unneeded noqas

--- ckanext/switzerland/helpers/terms_of_use_utils.py
+++ ckanext/switzerland/helpers/terms_of_use_utils.py
@@ -2,10 +2,10 @@
 import logging
 log = logging.getLogger(__name__)
 
-TERMS_OF_USE_OPEN = 'https://opendata.swiss/terms-of-use/#terms_open' # noqa
-TERMS_OF_USE_BY = 'https://opendata.swiss/terms-of-use#terms_by' # noqa
-TERMS_OF_USE_ASK = 'https://opendata.swiss/terms-of-use#terms_ask' # noqa
-TERMS_OF_USE_BY_ASK = 'https://opendata.swiss/terms-of-use#terms_by_ask' # noqa
+TERMS_OF_USE_OPEN = 'https://opendata.swiss/terms-of-use/#terms_open'
+TERMS_OF_USE_BY = 'https://opendata.swiss/terms-of-use#terms_by'
+TERMS_OF_USE_ASK = 'https://opendata.swiss/terms-of-use#terms_ask'
+TERMS_OF_USE_BY_ASK = 'https://opendata.swiss/terms-of-use#terms_by_ask'
 TERMS_OF_USE_CLOSED = 'ClosedData'
 
 OPEN_TERMS = [
