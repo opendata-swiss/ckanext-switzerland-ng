39b223a9ab45b085690f2d76f0719de62496a397
Rae Knowler <rae.knowler@liip.ch>
2025-02-11 17:02:24 +0100

    feat: Display all fields + label when adding a new relation

--- ckanext/switzerland/fanstatic/ogdch_form_helper_relations.js
+++ ckanext/switzerland/fanstatic/ogdch_form_helper_relations.js
@@ -5,8 +5,11 @@ ckan.module('ogdch_relations_add_rows', function ($) {
         var firstHiddenRows = $("[id^='field-relation-']")
           .parent('.controls')
           .parent('.form-group.ogdch-hide-row')
-          .slice(0, 2);
-        firstHiddenRows.addClass("ogdch-show-row");
+          .slice(0, 5);
+        firstHiddenRows.addClass('ogdch-show-row');
+        var firstHiddenLabel = $('.relation-label.ogdch-hide-row')
+            .slice(0, 1);
+        firstHiddenLabel.addClass('ogdch-show-row')
         $('.ogdch-show-row').removeClass('ogdch-hide-row');
         $( "#field-relation-title-1" ).focus();
       });
