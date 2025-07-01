76d6647266e031859c68e876440ce4a8552dda2f
kovalch <nataliia.kovalchuk@liip.ch>
2025-07-01 09:02:18 +0200

    feat: Modify setup file

--- setup.py
+++ setup.py
@@ -8,19 +8,21 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+version = '0.0.2'
+
 setup(
     name='''ckanext-switzerland''',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.0.1',
+    version=version,
 
     description='''DCAT-AP Switzerland and Scheming extensions''',
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/ogdch/ckanext-switzerland',
+    url='https://github.com/opendata-swiss/ckanext-switzerland-ng',
 
     # Author details
     author='''Liip AG''',
@@ -40,12 +42,14 @@ setup(
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
+        # Specify the Python versions you support here.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 
+    python_requires='>=3.6',
 
     # What does your project relate to?
     keywords='''CKAN schema dcat dcat-ap switzerland''',
