82680e01ec1fcbcce80f140c3aefd27e905181e7
Rae Knowler <rae.knowler@liip.ch>
2025-07-22 19:34:29 +0200

    tests: Use 'correct' egg name for ckanext-dcatapchharvest

--- bin/install_test_requirements.sh
+++ bin/install_test_requirements.sh
@@ -22,7 +22,8 @@ pip install -e git+https://github.com/ckan/ckanext-xloader.git#egg=ckanext-xload
 pip install -r https://raw.githubusercontent.com/ckan/ckanext-xloader/master/requirements.txt
 
 # Our ckanexts
-pip install -e git+https://github.com/opendata-swiss/ckanext-dcatapchharvest.git#egg=ckanext-dcatapchharvest
+# TODO: ckanext-dcatapchharvest has a typo that calls its egg "dcataapchharvest". Fix that and then fix it here.
+pip install -e git+https://github.com/opendata-swiss/ckanext-dcatapchharvest.git#egg=ckanext-dcataapchharvest
 pip install -r https://raw.githubusercontent.com/opendata-swiss/ckanext-dcatapchharvest/master/requirements.txt
 pip install -e git+https://github.com/opendata-swiss/ckanext-harvester_dashboard.git#egg=ckanext-harvester_dashboard
 pip install -r https://raw.githubusercontent.com/opendata-swiss/ckanext-harvester_dashboard/master/requirements.txt
